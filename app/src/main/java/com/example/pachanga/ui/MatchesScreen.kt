f6f857c5c027c977872de45892665eb8fe33abf4
Piotr Blaszczyk <piotrbpersonal@gmail.com>
2026-01-10 00:37:12 +0100

    add: match selector button

--- app/src/main/java/com/example/pachanga/ui/MatchesScreen.kt
+++ app/src/main/java/com/example/pachanga/ui/MatchesScreen.kt
@@ -14,15 +14,23 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,12 +40,16 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.pachanga.R
 import com.example.pachanga.data.PachangaDbHelper
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 
 @Composable
@@ -246,12 +258,20 @@ fun Team2 (
 @Composable
 fun MatchesScreen() {
     val context = LocalContext.current
-
     val team1 = remember { mutableStateListOf<Map<String, Any?>>() }
     val team2 = remember { mutableStateListOf<Map<String, Any?>>() }
+    var matches by remember { mutableStateOf<List<Map<String, Any?>>>(emptyList()) }
+    var selectedMatchIndex by remember { mutableIntStateOf(-1) }
 
     LaunchedEffect(Unit) {
-        val table = PachangaDbHelper(context).queryTable(tableName = "vw_match_stats", whereClause = "id_match = (SELECT MAX(id_match) from vw_match_stats)")
+        matches = PachangaDbHelper(context).queryTable(tableName = "vw_match").rows
+        selectedMatchIndex = matches.size - 1
+    }
+
+    LaunchedEffect(selectedMatchIndex) {
+        if (selectedMatchIndex == -1) return@LaunchedEffect
+        val idMatch = matches[selectedMatchIndex]["id"]
+        val table = PachangaDbHelper(context).queryTable(tableName = "vw_match_stats", whereClause = "id_match = $idMatch")
         team1.clear()
         team2.clear()
 
@@ -269,68 +289,147 @@ fun MatchesScreen() {
         }
     }
 
-    if (team1.isNotEmpty() && team2.isNotEmpty()){
-        val team1goals = team1.sumOf { it["goals"] as Int } + team2.sumOf { it["own_goals"] as Int }
-        val team2goals = team2.sumOf { it["goals"] as Int } + team1.sumOf { it["own_goals"] as Int }
-        Box(
-            modifier = Modifier
-                .safeDrawingPadding()
-                .fillMaxSize()
-        ){
-            Image(
-                painter = painterResource(R.drawable.football_field_bg),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-                    .border(
-                        BorderStroke(4.dp, Color.White),
-                    )
-            )
-            Column(
+
+
+
+    Column(
+        modifier = Modifier
+            .safeDrawingPadding()
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+
+        if (team1.isNotEmpty() && team2.isNotEmpty()){
+            val team1goals = team1.sumOf { it["goals"] as Int } + team2.sumOf { it["own_goals"] as Int }
+            val team2goals = team2.sumOf { it["goals"] as Int } + team1.sumOf { it["own_goals"] as Int }
+            Box(
                 modifier = Modifier
                     .safeDrawingPadding()
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Team1(
-                    players = team1,
-                    modifier = Modifier.weight(1f)
+                    .weight(1f)
+                    .fillMaxWidth()
+            ){
+                Image(
+                    painter = painterResource(R.drawable.football_field_bg),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                        .border(
+                            BorderStroke(4.dp, Color.White),
+                        )
                 )
-
                 Column(
                     modifier = Modifier
-                        .wrapContentHeight()
-                        .fillMaxWidth(),
+                        .safeDrawingPadding()
+                        .fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text(team1goals.toString(),
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    Team1(
+                        players = team1,
+                        modifier = Modifier.weight(1f)
                     )
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(team2goals.toString(),
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
+
+                    Column(
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(team1goals.toString(),
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(team2goals.toString(),
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                    }
+
+                    Team2(
+                        players = team2,
+                        modifier = Modifier.weight(1f)
                     )
                 }
-
-                Team2(
-                    players = team2,
-                    modifier = Modifier.weight(1f)
-                )
+            }
+        } else {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Loading Match...")
             }
         }
-    } else {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "Loading Match...")
+        if (matches.isNotEmpty()){
+            MatchSelectorButton(
+                selectedIndex = selectedMatchIndex,
+                onSelectedIndexChange = { newIndex -> selectedMatchIndex = newIndex },
+                matches = matches,
+                modifier = Modifier.fillMaxWidth().heightIn(max = 40.dp).padding(horizontal = 4.dp)
+            )
         }
     }
+}
+@Composable
+fun MatchSelectorButton(
+    matches: List<Map<String,Any?>>,
+    selectedIndex: Int,
+    onSelectedIndexChange: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val dateFormatIn = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    val dateFormatOut = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.forLanguageTag("es-ES"))
+    val prevIndex = selectedIndex - 1
+    val nextIndex = selectedIndex + 1
 
+    SingleChoiceSegmentedButtonRow(modifier) {
+        SegmentedButton(
+            shape = SegmentedButtonDefaults.itemShape(
+                index = 1,
+                count = 3
+            ),
+            onClick = {onSelectedIndexChange(prevIndex)},
+            selected = false,
+            enabled = prevIndex >= 0,
+            label = {
+                matches.getOrNull(prevIndex)?.let {
+                    Text(LocalDateTime.parse( matches[prevIndex]["datetime"] as String, dateFormatIn).format(dateFormatOut),
+                        maxLines = 1,
+                        overflow = TextOverflow.Visible,
+                        softWrap = false
+                        )
+                }
+            },
+        )
+        SegmentedButton(
+            shape = SegmentedButtonDefaults.itemShape(
+                index = 1,
+                count = 3
+            ),
+            onClick = { },
+            icon = {  },
+            selected = true,
+            label = {
+                Text(LocalDateTime.parse( matches[selectedIndex]["datetime"] as String, dateFormatIn).format(dateFormatOut))
+            }
+        )
+        SegmentedButton(
+            shape = SegmentedButtonDefaults.itemShape(
+                index = 1,
+                count = 3
+            ),
+            onClick = { onSelectedIndexChange(nextIndex) },
+            selected = false,
+            enabled = nextIndex < matches.size,
+            label = {
+                matches.getOrNull(nextIndex)?.let {
+                    Text(LocalDateTime.parse( matches[nextIndex]["datetime"] as String, dateFormatIn).format(dateFormatOut))
+                }
+            }
+        )
+    }
 }
