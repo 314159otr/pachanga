53c234df0cbf4d3d952853e94ee9f50b5410c71f
Piotr Blaszczyk <piotrbpersonal@gmail.com>
2026-01-17 09:19:56 +0100

    wip

--- app/src/main/java/com/example/pachanga/ui/MainScreen.kt
+++ app/src/main/java/com/example/pachanga/ui/MainScreen.kt
@@ -2,6 +2,7 @@ package com.example.pachanga.ui
 
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
@@ -17,10 +18,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.pachanga.shared.Constants
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.NavHost
+import com.example.pachanga.shared.MyNavigationBar
 
 @Composable
 fun MainScreen() {
@@ -29,7 +32,7 @@ fun MainScreen() {
     var selectedScreen by rememberSaveable { mutableIntStateOf(startScreen.ordinal) }
     Scaffold(
         bottomBar = {
-            NavigationBar(windowInsets = WindowInsets.navigationBars) {
+            MyNavigationBar(windowInsets = WindowInsets.navigationBars,) {
                 Constants.NavBarScreen.entries.forEachIndexed { index, screen ->
                     NavigationBarItem(
                         selected = selectedScreen == index,
@@ -43,9 +46,7 @@ fun MainScreen() {
                                 contentDescription = screen.label
                             )
                         },
-                        label = {
-                            Text(screen.label)
-                        }
+                        alwaysShowLabel = false,
                     )
                 }
 
