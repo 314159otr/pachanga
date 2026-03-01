930305da029a398d0d237804aaf1ce6ce087e59b
Piotr Blaszczyk <piotrbpersonal@gmail.com>
2026-03-01 22:56:00 +0100

    theming base

--- app/src/main/java/com/example/pachanga/MainActivity.kt
+++ app/src/main/java/com/example/pachanga/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -12,25 +13,33 @@ import androidx.navigation.compose.rememberNavController
 import com.example.pachanga.shared.Constants
 import com.example.pachanga.ui.GreetingScreen
 import com.example.pachanga.ui.MainScreen
+import com.example.pachanga.ui.theme.PachangaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.light(
-                scrim = android.graphics.Color.TRANSPARENT,
-                darkScrim = android.graphics.Color.TRANSPARENT,
-            )
-        )
         setContent {
-            val rootNavController = rememberNavController()
-
-            NavHost(
-                navController = rootNavController,
-                startDestination = Constants.RootRoutes.GREETING) {
-                composable(Constants.RootRoutes.GREETING) { GreetingScreen(rootNavController) }
-                composable(Constants.RootRoutes.MAIN_CONTAINER) { MainScreen() }
+            enableEdgeToEdge(
+                statusBarStyle = if (isSystemInDarkTheme()) {
+                    SystemBarStyle.dark(
+                        scrim = android.graphics.Color.TRANSPARENT
+                    )
+                } else {
+                    SystemBarStyle.light(
+                        scrim = android.graphics.Color.TRANSPARENT,
+                        darkScrim = android.graphics.Color.TRANSPARENT
+                    )
+                }
+            )
+            PachangaTheme(dynamicColor = false) {
+                val rootNavController = rememberNavController()
+                NavHost(
+                    navController = rootNavController,
+                    startDestination = Constants.RootRoutes.GREETING) {
+                    composable(Constants.RootRoutes.GREETING) { GreetingScreen(rootNavController) }
+                    composable(Constants.RootRoutes.MAIN_CONTAINER) { MainScreen() }
+                }
             }
         }
     }
